9f7605c303ae51441d8fc839ecc3de2cec1ab84e
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-29 16:07:17 +0530

    final

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,10 +9,12 @@ import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import { useStateValue } from "../StateProvider";
+import { Button, IconButton } from "@material-ui/core";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 function Sidebar() {
   const [{ user }, dispatch] = useStateValue();
   return (
-    <div className="sidebar">
+    <div id="top" className="sidebar">
       <SidebarRow src={user.photoURL} title={user.displayName} />
       <SidebarRow Icon={AddBoxIcon} title="COVID-19 Information Center" />
       <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
@@ -21,6 +23,11 @@ function Sidebar() {
       <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
       <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
       <SidebarRow Icon={ExpandMoreIcon} title="Marketplace" />
+      <div className="top">
+        <Button className="top__button" href="#top">
+          <ArrowUpwardIcon style={{ color: "white" }} />
+        </Button>
+      </div>
     </div>
   );
 }
